199debd431eceed2e620a6b0789844077344889e
huonggiang1412 <161736452+huonggiang1412@users.noreply.github.com>
2024-04-04 19:30:07 +0700

    first commit

--- src/view/SinhVienView.java
+++ src/view/SinhVienView.java
@@ -3,7 +3,6 @@ package view;
 import java.util.Scanner;
 
 import controller.SinhVienController;
-import controller.FileIOThread;
 
 public class SinhVienView {
     private SinhVienController svController;
@@ -14,8 +13,8 @@ public class SinhVienView {
 
     public void hienThiMenu() {
         System.out.println("----- Quản lý mượn trả sách -----");
-        System.out.println("1. Hiển thị danh sách thẻ mượn");
-        System.out.println("2. Thêm thẻ mượn");
+        System.out.println("1. Thêm thẻ mượn");        
+        System.out.println("2. Hiển thị danh sách thẻ mượn");
         System.out.println("3. Xóa thẻ mượn");
         System.out.println("4. Lưu danh sách thẻ mượn xuống file");
         System.out.println("5. Tải danh sách thẻ mượn từ file");
@@ -27,23 +26,19 @@ public class SinhVienView {
         Scanner scanner = new Scanner(System.in);
         switch (luaChon) {
             case 1:
-                svController.hienThiThongTin();
+            	svController.themTheMuon(scanner);
                 break;
             case 2:
-                svController.themTheMuon(scanner);
+                svController.hienThiThongTin();
                 break;
             case 3:
                 svController.xoaTheMuon(scanner);
                 break;
             case 4:
-                // Tạo và bắt đầu một luồng mới để thực hiện IO
-                FileIOThread fileIOThread = new FileIOThread(svController);
-                fileIOThread.start();
+               svController.TestLuuFile();
                 break;
             case 5:
-                // Tạo và bắt đầu một luồng mới để thực hiện IO
-                FileIOThread fileIOThread2 = new FileIOThread(svController);
-                fileIOThread2.start();
+            	svController.TestDocFile();
                 break;
             case 0:
                 System.out.println("Thoát chương trình.");
